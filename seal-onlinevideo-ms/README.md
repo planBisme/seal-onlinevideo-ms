ae3c312e0cdaf7dfa3440099697de91f525e01bb
lymj <320629576@qq.com>
2023-04-05 20:58:00 +0800

    Initial commit

---
+++ seal-onlinevideo-ms/README.md
@@ -0,0 +1,2 @@
+# seal-onlinevideo-ms
+
---
+++ seal-onlinevideo-ms/src/main/java/com/seal/SealOnlinevideoMsDemoApp.java
@@ -0,0 +1,13 @@
+package com.seal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SealOnlinevideoMsDemoApp {
+
+    public static void main(String[] args){
+        ApplicationContext app = SpringApplication.run(SealOnlinevideoMsDemoApp.class, args);
+    }
+}
